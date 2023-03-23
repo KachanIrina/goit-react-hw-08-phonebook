f73ead26a7ca861ae61a4a32b1bf1c8c221c9df0
KachanIrina <106675851+KachanIrina@users.noreply.github.com>
2023-03-23 13:52:18 +0000

    Deploying to gh-pages from @ KachanIrina/goit-react-hw-08-phonebook@51703ff183b0b2c8722a646db6b7a4ba7cdc18e1 🚀

---
+++ static/js/415.09201024.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[415],{5415:function(e,t,n){n.r(t),n.d(t,{default:function(){return i}});var r=n(184),c={container:{display:"flex",alignItems:"center",justifyContent:"center",minHeight:"calc(70vh - 60px)"},title:{fontWeight:500,fontSize:48,textAlign:"center",color:"rgb(19, 13, 8)"}};function i(){return(0,r.jsx)("div",{style:c.container,children:(0,r.jsx)("h1",{style:c.title,children:"Welcome to the Phonebook!"})})}}}]);
+//# sourceMappingURL=415.09201024.chunk.js.map
